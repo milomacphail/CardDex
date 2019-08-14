0af7299514902190f52f7202d28e9ebb8ff7d786
milomacphail <milomacphail@gmail.com>
2019-08-14 19:20:12 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CardDex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
